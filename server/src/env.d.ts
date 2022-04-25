ba3aa82ff7c06dbb683588e73136e4e73142d283
Alvaro Castillo <alvcastillo@paypal.com>
2022-04-25 04:39:12 -0500

    fix: removing lock files and improvements on the backend

--- server/src/env.d.ts
+++ server/src/env.d.ts
@@ -2,9 +2,7 @@ declare global {
   namespace NodeJS {
     interface ProcessEnv {
       DATABASE_URL: string;
-      REDIS_URL: string;
       CORS_ORIGIN: string;
-      SESSION_SECRET: string;
       PORT: string;
     }
   }
